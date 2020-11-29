5a91225078e527097fa1f25ad6ee10871e15c3f2
Alanis Pereyra <alaniswagner14@hotmail.com>
2020-11-28 17:08:13 -0800

    Test case decimal 1001

--- test.rb
+++ test.rb
@@ -4,7 +4,7 @@ require './Decimal_To_Binary'
 #Test cases
 # [X] 3 -> 1010 (11)
 # [X] 14 -> 1110
-# [] 98 -> 101000 (1100010)
+# [X] 98 -> 101000 (1100010)
 # [] 1001 -> 10010110
 # [] 2505 -> 101010001
 
@@ -20,5 +20,9 @@ class ConvertTest < Test::Unit::TestCase
     def test_3
         test_3 = decimal_to_binary(98)
         assert_equal "1100010", test_3
+    end
+    def test_4 
+        test_4 = decimal_to_binary(1001)
+        assert_equal "1111101001", test_4
     end
 end
